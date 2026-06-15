aecfe64634faa9ac7c89b1b12521eaa65365dc7b
Harsh Kumar <hk.develop07@gmail.com>
2026-06-16 01:03:34 +0530

    setup of chat page and socket.io in chat

--- src/app/chat/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function page (){
-  return (
-    <div> chat page</div>
-  )
-}
-
---
+++ src/types/chat.ts
@@ -0,0 +1,12 @@
+
+export interface chatType{
+    _id:string;
+    text:string;
+    senderId:{
+        firstName:string;
+        _id:string;
+    }
+    createdAt:string;
+    updatedAt:string;
+    
+}
